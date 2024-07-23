187192bb342076326d97a8fe514a2978ace575ed
WillianNalbert <williannalbert@hotmail.com>
2024-07-22 22:03:50 -0300

    inclusão do gitignore

--- SistemaDeAgendamentos/obj/Debug/net8.0/SistemaDeAgendamentos.AssemblyInfo.cs
+++ SistemaDeAgendamentos/obj/Debug/net8.0/SistemaDeAgendamentos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeAgendamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd11e097bdb5cade6bccf5be1e3c094c89bfd59e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeAgendamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeAgendamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
